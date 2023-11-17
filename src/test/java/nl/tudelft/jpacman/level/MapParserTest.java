7cea3854e81a2cd72bb93cd208432a48f504e08b
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-11-17 15:52:00 +0700

    test: JPAC-0 parse map wrong

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,9 +1,11 @@
 package nl.tudelft.jpacman.level;
 
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.Board;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -43,6 +45,29 @@ public class MapParserTest {
         Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
         Mockito.verify(boardFactory, Mockito.times(26)).createWall();
         Mockito.verify(boardFactory, Mockito.times(10)).createGround();
+    }
+
+    /**
+     * Test for the parseMap method with an incorrectly formatted map.
+     */
+    @Test
+    public void testParseMapWrong1() {
+        MockitoAnnotations.initMocks(this);
+        assertNotNull(boardFactory);
+        assertNotNull(levelFactory);
+        MapParser mapParser = new MapParser(levelFactory, boardFactory);
+        ArrayList<String> map = new ArrayList<>();
+        map.add("########");
+        map.add("#P    G#");
+        map.add("####"); // Inconsistent length
+        map.add("#P @ G#"); // Invalid character '@'
+
+        PacmanConfigurationException thrown = Assertions.assertThrows(
+            PacmanConfigurationException.class, () -> mapParser.parseMap(map)
+        );
+
+        String expectedErrorMessage = "Input text lines are not of equal width.";
+        Assertions.assertEquals(expectedErrorMessage, thrown.getMessage());
     }
 
 }
